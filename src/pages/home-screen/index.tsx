de3561d1e6c0ccf0389542aa2c6b83202602e737
rasult22 <warrior.006@inbox.ru>
2025-12-05 20:35:53 +0500

    feat(home-screen): add header and loading state to application button
    Add a header section with title and button to home screen. Update application button to show loading state during mutation to improve user feedback.

--- src/pages/home-screen/index.tsx
+++ src/pages/home-screen/index.tsx
@@ -40,15 +40,22 @@ export const HomeScreen = () => {
                 <motion.div 
                     layout
                     transition={{ duration: 0.5, ease: 'easeInOut' }}
-                    className={`border-l border-border-primary w-full`}
+                    className={`border-l border-border-primary w-full py-8`}
                 >
+                {/* header */}
+                <div className='px-8 flex justify-between items-center'>
+                    <div className='font-semibold text-[24px] leading-[32px]'>
+                        Applications
+                    </div>
+                    <Button isLoading={createApplicationMutation.isPending} iconLeading={Plus} size="lg" onClick={handleStart}>Submit an application</Button>
+                </div>
                   <div className="flex flex-col w-full bg-primary h-full items-center justify-center p-8">
                         <div className="text-center space-y-4">
                             <h3 className="text-xl font-semibold text-primary"> Готовы открыть свою компанию в IFZA?</h3>
                             <p className="text-md text-tertiary">
                             Заполните заявку прямо сейчас через Adlex AI
                             </p>
-                            <Button iconLeading={Plus} size="lg" onClick={handleStart}>
+                            <Button isLoading={createApplicationMutation.isPending} iconLeading={Plus} size="lg" onClick={handleStart}>
                                 Submit an application
                             </Button>
                         </div>
